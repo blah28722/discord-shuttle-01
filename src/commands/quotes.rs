8ee5a60e1094b2c256c1c290bac55fa06d2cfa73
John Chen <john.hk.chen@gmail.com>
2023-12-24 14:13:51 -0800

    Clean up quotes code

--- src/commands/quotes.rs
+++ src/commands/quotes.rs
@@ -1,31 +1,18 @@
-use anyhow::Result;
-use serde::{Deserialize, Serialize};
+use anyhow::{bail, Result};
+use serde::Deserialize;
 
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize)]
 struct Quote {
-    _id: String,
     content: String,
     author: String,
-    tags: Vec<String>,
-    authorSlug: String,
-    length: u32,
-    dateAdded: String,
-    dateModified: String,
 }
 
-#[allow(dead_code)]
 pub fn generate_quote() -> Result<String> {
     let url = "https://api.quotable.io/quotes/random";
-    let data = reqwest::blocking::get(url)?.text()?;
-    let payload = serde_json::from_str::<Vec<Quote>>(data.as_str())?;
+    let payload = reqwest::blocking::get(url)?.json::<Vec<Quote>>()?;
     match payload.get(0) {
-        None => Ok(format!("<Error: missing/bad response from API server>")),
-        Some(quote) => {
-            let content = &quote.content;
-            let author = &quote.author;
-            Ok(format!("\"{content}\"\n\n\t\t~{author}"))
-        }
+        None => bail!("Missing/unexpected quote from API server."),
+        Some(q) => Ok(format!("\"{}\"\n\n\t\t~{}", q.content, q.author)),
     }
 }
 
@@ -33,17 +20,13 @@ pub fn generate_quote() -> Result<String> {
 mod tests {
     use super::*;
 
-    #[ignore]
+    // #[ignore]
     #[test]
     fn it_fetches_quote_json() {
         let ans = generate_quote();
         match ans {
-            Err(e) => {
-                println!("Got some error! {e}");
-            }
-            Ok(s) => {
-                println!("Got a result: \n{s}");
-            }
+            Err(e) => println!("Got some error! {e}"),
+            Ok(s) => println!("Got a result: \n{s}"),
         }
     }
 }
